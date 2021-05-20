f6d558d377816beb6b63666014dc8d922cc0610a
h3ftw <83728002+h3ftw@users.noreply.github.com>
2021-05-20 10:40:45 +0000

    Added H2 STemplateEntityFactory (TEMP) file support
    5/19/2021 - [REDACTED] Manually added resource type TEMPH2 and classes STemplateEntityFactoryH2 and STemplateFactorySubEntityH2 for H2 STemplateEntityFactory (TEMP) file support

--- Tools/ResourceTool/Src/Resources.cpp
+++ Tools/ResourceTool/Src/Resources.cpp
@@ -1,3 +1,6 @@
+/*
+	5/19/2021 - [REDACTED] Manually added resource type TEMPH2 and classes STemplateEntityFactoryH2 and STemplateFactorySubEntityH2 for H2 STemplateEntityFactory (TEMP) file support
+*/
 #include "Resources.h"
 
 #include "ResourceConverter.h"
@@ -9,6 +12,7 @@
 // Register all supported resource types here.
 std::unordered_map<std::string, Resource> g_Resources = {
 	REGISTER_RESOURCE(TEMP, STemplateEntityFactory)
+	REGISTER_RESOURCE(TEMPH2, STemplateEntityFactoryH2)
 	REGISTER_RESOURCE(TBLU, STemplateEntityBlueprint)
 	REGISTER_RESOURCE(AIRG, SReasoningGrid)
 	//REGISTER_RESOURCE(ASVA, SPackedAnimSetEntry)
