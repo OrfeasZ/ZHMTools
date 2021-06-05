9fafc9e873607fdb9f12d8b0bb125cf903efbff9
OrfeasZ <orfeaz@gmail.com>
2021-06-05 23:55:26 +0300

    Improve navmesh viewing tool

--- Tools/NavViewer/viewer.js
+++ Tools/NavViewer/viewer.js
@@ -3,17 +3,21 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+
+const rendererElement = document.getElementById('render-canvas');
+rendererElement.replaceWith(renderer.domElement);
+
+const mapSelector = document.getElementById('map-selector');
+
+const checkRadii = document.getElementById('check-radii');
+const checkCenters = document.getElementById('check-centers');
+const checkFaces = document.getElementById('check-faces');
+const checkLines = document.getElementById('check-lines');
+const checkMarkedVertices = document.getElementById('check-marked-vertices');
 
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-const size = 1000;
-const divisions = 1000;
-
-const gridHelper = new THREE.GridHelper(size, divisions);
-//scene.add(gridHelper);
-
-function renderTriangle(v1, v2, v3) {
+function renderTriangle(v1, v2, v3, color) {
     const geometry = new THREE.BufferGeometry();
 
     const vertices = [
@@ -24,15 +28,15 @@ function renderTriangle(v1, v2, v3) {
 
     geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(vertices), 3));
 
-    const meshMaterial = new THREE.MeshBasicMaterial({ color: 0xFF69B4, side: THREE.DoubleSide, opacity: 0.3, transparent: true, blending: THREE.AdditiveBlending });
+    const meshMaterial = new THREE.MeshBasicMaterial({ color, side: THREE.DoubleSide, opacity: 0.5, transparent: true, blending: THREE.AdditiveBlending });
     const mesh = new THREE.Mesh(geometry, meshMaterial);
 
     scene.add(mesh);
 }
 
-function renderFace(edges) {
+function renderFace(edges, color) {
     for (let i = 0; i < edges.length - 2; ++i) {
-        renderTriangle(edges[0], edges[i + 1], edges[i + 2]);
+        renderTriangle(edges[0], edges[i + 1], edges[i + 2], color);
     }
 }
 
@@ -47,15 +51,80 @@ function renderOutline(edges) {
 
     const geometry = new THREE.BufferGeometry().setFromPoints(points);
 
-    const lineMaterial = new THREE.LineBasicMaterial({ color: 0xFF69B4 });
+    const lineMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00 });
     const line = new THREE.Line(geometry, lineMaterial);
 
     scene.add(line);
 }
 
-for (const face of Faces) {
-    renderFace(face);
-    renderOutline(face);
+function renderSurfaceRadius(centerX, centerY, centerZ, maxRadius) {
+    const geometry = new THREE.CircleGeometry(maxRadius, 32);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, side: THREE.DoubleSide, opacity: 0.3, transparent: true });
+
+    const circle = new THREE.Mesh(geometry, material);
+    circle.position.set(centerY, centerZ - 0.01, centerX);
+    circle.rotation.set(1.5708, 0, 0);
+
+    scene.add(circle);
+}
+
+function renderSurfaceCenter(centerX, centerY, centerZ) {
+    const geometry = new THREE.CircleGeometry(0.1, 5);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide });
+
+    const circle = new THREE.Mesh(geometry, material);
+    circle.position.set(centerY, centerZ + 0.01, centerX);
+    circle.rotation.set(1.5708, 0, 0);
+
+    scene.add(circle);
+}
+
+function renderMarkedVertex(vertex) {
+    const geometry = new THREE.CircleGeometry(0.1, 5);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff00ff, side: THREE.DoubleSide });
+
+    const circle = new THREE.Mesh(geometry, material);
+    circle.position.set(vertex[1], vertex[2] + 0.01, vertex[0]);
+    circle.rotation.set(1.5708, 0, 0);
+
+    scene.add(circle);
+}
+
+function renderSurface(surface) {
+    const centerX = surface[0];
+    const centerY = surface[1];
+    const centerZ = surface[2];
+    const maxRadius = surface[3];
+
+    const isSteps = surface[4] === 8;
+    const vertexIndex = surface[5];
+    const vertices = surface[6];
+
+    if (checkRadii.checked) {
+        renderSurfaceRadius(centerX, centerY, centerZ, maxRadius);
+    }
+
+    if (checkCenters.checked) {
+        renderSurfaceCenter(centerX, centerY, centerZ);
+    }
+
+    let faceColor = 0x00FF00;
+
+    if (isSteps) {
+        faceColor = 0xFF69B4;
+    }
+
+    if (checkFaces.checked) {
+        renderFace(vertices, faceColor);
+    }
+    
+    if (checkLines.checked) {
+        renderOutline(vertices);
+    }
+
+    if (checkMarkedVertices.checked) {
+        renderMarkedVertex(vertices[vertexIndex]);
+    }
 }
 
 camera.position.set(-40, 50, 80);
@@ -68,4 +137,44 @@ function animate() {
 
     renderer.render(scene, camera);
 }
+
 animate();
+
+function reRender() {
+    // Clear the scene.
+    while (scene.children.length > 0) {
+        scene.remove(scene.children[0]);
+    }
+
+    // Render everything.
+    const selectedMap = mapSelector.value;
+
+    console.log('Rendering ' + selectedMap);
+    
+    for (const surface of Surfaces[selectedMap]) {
+        renderSurface(surface);
+    }
+}
+
+let hasSelection = false;
+
+for (const mapName in Surfaces) {
+    const optionElement = document.createElement('option');
+    optionElement.innerHTML = mapName;
+
+    if (!hasSelection) {
+        optionElement.selected = true;
+        hasSelection = true;
+    }
+
+    mapSelector.appendChild(optionElement);
+}
+
+reRender();
+
+mapSelector.addEventListener('change', () => reRender());
+checkRadii.addEventListener('change', () => reRender());
+checkCenters.addEventListener('change', () => reRender());
+checkFaces.addEventListener('change', () => reRender());
+checkLines.addEventListener('change', () => reRender());
+checkMarkedVertices.addEventListener('change', () => reRender());
