e6818ab5241ebacc0f5fa05dc2ec1285d5b14207
OrfeasZ <orfeaz@gmail.com>
2022-02-21 22:05:58 +0200

    Add web-based version of ResourceTool

--- Libraries/ResourceLib/Include/Embind.h
+++ Libraries/ResourceLib/Include/Embind.h
@@ -39,15 +39,37 @@ val ConvertResourceToJson(const std::string& p_ResourceType, const std::string& 
 		return val::null();
 
 	auto* s_Result = s_Converter->FromMemoryToJsonString(p_ResourceData.data(), p_ResourceData.size());
-	
+
 	if (s_Result == nullptr)
 		return val::null();
-	
+
 	std::string s_JsonData(s_Result->JsonData, s_Result->StrSize);
-	
+
 	s_Converter->FreeJsonString(s_Result);
-	
+
 	return val(s_JsonData);
+}
+
+val GenerateResourceFromJson(const std::string& p_ResourceType, const std::string& p_ResourceData)
+{
+	auto* s_Generator = ZHM_TARGET_FUNC(GetGeneratorForResource)(p_ResourceType.c_str());
+
+	if (s_Generator == nullptr)
+		return val::null();
+
+	auto* s_Result = s_Generator->FromJsonStringToResourceMem(p_ResourceData.data(), p_ResourceData.size(), false);
+
+	if (s_Result == nullptr)
+		return val::null();
+
+	emscripten::val s_View(emscripten::typed_memory_view(s_Result->DataSize, reinterpret_cast<const uint8_t*>(s_Result->ResourceData)));
+
+	auto s_JSResult = emscripten::val::global("Uint8Array").new_(s_Result->DataSize);
+	s_JSResult.call<void>("set", s_View);
+
+	s_Generator->FreeResourceMem(s_Result);
+
+	return s_JSResult;
 }
 
 EMSCRIPTEN_BINDINGS(ZHM_MODULE_NAME)
@@ -57,6 +79,7 @@ EMSCRIPTEN_BINDINGS(ZHM_MODULE_NAME)
 	function("GetSupportedResourceTypes", &GetSupportedResourceTypes);
 	function("IsResourceTypeSupported", &IsResourceTypeSupported);
 	function("ConvertResourceToJson", &ConvertResourceToJson);
+	function("GenerateResourceFromJson", &GenerateResourceFromJson);
 }
 
 #endif
---
+++ docs/ResourceTool.js
@@ -0,0 +1,266 @@
+(async () => {
+	const domLoading = document.querySelector('#loading');
+	const domGameSelector = document.querySelector('#game-selector');
+	const domResourceConverter = document.querySelector('#resource-converter');
+	const domHm3 = document.querySelector('#hm3');
+	const domHm2 = document.querySelector('#hm2');
+	const domHm2016 = document.querySelector('#hm2016');
+	const domHma = document.querySelector('#hma');
+	const domBack = document.querySelector('#back');
+	const domConvertJson = document.querySelector('#convert-json');
+	const domGenerateResource = document.querySelector('#generate-resource');
+
+	let selectedGame = null;
+
+	const HM3 = await LoadHM3();
+	const HM2 = await LoadHM2();
+	const HM2016 = await LoadHM2016();
+	const HMA = await LoadHMA();
+
+	const selectGame = (game) => {
+		game = game.toLowerCase();
+
+		// Set the correct game module.
+		if (game === 'hm3') {
+			selectedGame = HM3;
+		} else if (game === 'hm2') {
+			selectedGame = HM2;
+		} else if (game === 'hm2016') {
+			selectedGame = HM2016;
+		} else if (game === 'hma') {
+			selectedGame = HMA;
+		} else {
+			// Invalid game.
+			return;
+		}
+
+		// Set game in URL.
+		const newUrl = new URL(window.location);
+		newUrl.hash = game;
+		history.pushState({}, '', newUrl);
+
+		// Show the converter dialog.
+		domGameSelector.style.display = 'none';
+		domResourceConverter.style.display = 'block';
+	};
+
+	const goToGameSelection = () => {
+		domResourceConverter.style.display = 'none';
+		domGameSelector.style.display = 'block';
+
+		const newUrl = new URL(window.location);
+		newUrl.hash = '';
+		history.pushState({}, '', newUrl);
+	};
+
+	const getFileFromEvent = (e) => {
+		if (e.dataTransfer.items) {
+			for (let i = 0; i < e.dataTransfer.items.length; ++i) {
+				if (e.dataTransfer.items[i].kind === 'file') {
+					return e.dataTransfer.items[i].getAsFile();
+				}
+			}
+		} else {
+			for (let i = 0; i < e.dataTransfer.files.length; ++i) {
+				return ev.dataTransfer.files[i];
+			}
+		}
+
+		return null;
+	};
+
+	const getFileExtension = (file) => {
+		const fileNameParts = file.name.split('.');
+		return fileNameParts[fileNameParts.length - 1];
+	};
+
+	const getSecondFileExtension = (file) => {
+		const fileNameParts = file.name.split('.');
+
+		if (fileNameParts.length < 3) {
+			return null;
+		}
+
+		return fileNameParts[fileNameParts.length - 2];
+	};
+
+	const saveFile = (data, name) => {
+		const blob = new Blob([ data ], { 
+			type: 'application/octet-stream',
+		});
+
+		const url = URL.createObjectURL(blob);
+
+		const link = document.createElement('a');
+		link.style.display = 'none';
+		link.href = url;
+		link.download = name;
+
+		document.body.appendChild(link);
+		link.click();
+
+		setTimeout(() => {
+			document.body.removeChild(link);
+			window.URL.revokeObjectURL(url);
+		}, 0);
+	};
+
+	const convertToJson = (file) => {
+		const fileName = file.name;
+		const resourceType = getFileExtension(file).toUpperCase();
+
+		if (!selectedGame.IsResourceTypeSupported(resourceType)) {
+			console.error('Unsupported resource type: ' + resourceType);
+			return;
+		}
+
+		// Show loading indicator.
+		domResourceConverter.style.display = 'none';
+		domLoading.style.display = 'block';
+
+		file.arrayBuffer().then((buffer) => {
+			const json = selectedGame.ConvertResourceToJson(resourceType, buffer);
+
+			domLoading.style.display = 'none';
+			domResourceConverter.style.display = 'block';
+
+			if (json === null) {
+				console.log('Failed to convert resource to json.');
+				return;
+			}
+
+			saveFile(json, fileName + '.json');
+		});
+	};
+
+	const convertFromJson = (file) => {
+		const fileName = file.name;
+		let resourceType = getSecondFileExtension(file);
+
+		if (resourceType === null) {
+			console.error('No resource type in file name.');
+			return;
+		}
+
+		resourceType = resourceType.toUpperCase();
+
+		if (getFileExtension(file).toLowerCase() != 'json') {
+			console.error('Can only generate resources from json files.');
+			return;
+		}
+
+		if (!selectedGame.IsResourceTypeSupported(resourceType)) {
+			console.error('Unsupported resource type: ' + resourceType);
+			return;
+		}
+
+		// Show loading indicator.
+		domResourceConverter.style.display = 'none';
+		domLoading.style.display = 'block';
+
+		file.arrayBuffer().then((buffer) => {
+			const resourceData = selectedGame.GenerateResourceFromJson(resourceType, buffer);
+
+			domLoading.style.display = 'none';
+			domResourceConverter.style.display = 'block';
+
+			if (resourceData === null) {
+				console.log('Failed to generate resource from json.');
+				return;
+			}
+
+			saveFile(resourceData, fileName.substr(0, fileName.length - 5));
+		});
+	};
+
+	// Button handlers.
+	domHm3.addEventListener('click', (e) => {
+		e.preventDefault();
+		selectGame('HM3');
+	});
+	
+	domHm2.addEventListener('click', (e) => {
+		e.preventDefault();
+		selectGame('HM2');
+	});
+	
+	domHm2016.addEventListener('click', (e) => {
+		e.preventDefault();
+		selectGame('HM2016');
+	});
+	
+	domHma.addEventListener('click', (e) => {
+		e.preventDefault();
+		selectGame('HMA');
+	});
+	
+	domBack.addEventListener('click', (e) => {
+		e.preventDefault();
+		goToGameSelection();
+	});
+
+	// Resource to JSON handlers.
+	domConvertJson.addEventListener('dragover', (e) => {
+		e.preventDefault();
+	});
+
+	domConvertJson.addEventListener('drop', (e) => {
+		e.preventDefault();
+		domConvertJson.classList.remove('over');
+
+		const file = getFileFromEvent(e);
+
+		if (file === null) {
+			return;
+		}
+
+		convertToJson(file);
+	});
+
+	domConvertJson.addEventListener('dragenter', (e) => {
+		e.preventDefault();
+		domConvertJson.classList.add('over');
+	});
+
+	domConvertJson.addEventListener('dragleave', (e) => {
+		e.preventDefault();
+		domConvertJson.classList.remove('over');
+	});
+
+	// JSON to Resource handlers.
+	domGenerateResource.addEventListener('dragover', (e) => {
+		e.preventDefault();
+	});
+
+	domGenerateResource.addEventListener('drop', (e) => {
+		e.preventDefault();
+		domGenerateResource.classList.remove('over');
+
+		const file = getFileFromEvent(e);
+
+		if (file === null) {
+			return;
+		}
+
+		convertFromJson(file);
+	});
+
+	domGenerateResource.addEventListener('dragenter', (e) => {
+		e.preventDefault();
+		domGenerateResource.classList.add('over');
+	});
+
+	domGenerateResource.addEventListener('dragleave', (e) => {
+		e.preventDefault();
+		domGenerateResource.classList.remove('over');
+	});
+
+	// Show game selector.
+	domLoading.style.display = 'none';
+	domGameSelector.style.display = 'block';
+
+	// Check if we already have a game specified in the URL.
+	if (document.location.hash.length > 1) {
+		selectGame(document.location.hash.substr(1));
+	}
+})();
